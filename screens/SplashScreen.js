d4185754494387102c5a446e7c867899e7efce3d
Yasas Dilshan Ekanayaka <ydilshan.ek@gmail.com>
2020-07-11 01:24:23 +0530

    Update SplashScreen.js

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -28,21 +28,26 @@ const SplashScreen = ({navigation}) => {
           style={styles.logo}
           resizeMode="stretch"
         />
+        <Text style={styles.title}>LAVA FOODS</Text>
       </View>
       <Animatable.View
         style={[styles.footer, {backgroundColor: '#fff'}]}
         animation="fadeInUpBig">
-        <Text style={styles.title}>Stay Connected with Everyone</Text>
-        <Text style={styles.text}>Sign in with account</Text>
-        <View style={styles.button}>
-          <TouchableOpacity onPress={() => navigation.navigate('NavScreen')}>
-            <LinearGradient
-              colors={['#08d4c4', '#01ab9d']}
-              style={styles.signIn}>
-              <Text style={styles.textSign}>Get Started</Text>
-              <MaterialIcons name="navigate-next" color="#fff" size={20} />
-            </LinearGradient>
-          </TouchableOpacity>
+        <View style={styles.views}>
+          <View style={styles.views2}>
+            <Text style={styles.title2}>Eat & Sell</Text>
+            <Text style={styles.text}>Sign in with account</Text>
+          </View>
+          <View style={styles.button}>
+            <TouchableOpacity onPress={() => navigation.navigate('NavScreen')}>
+              <LinearGradient
+                colors={['#08d4c4', '#01ab9d']}
+                style={styles.signIn}>
+                <Text style={styles.textSign}>Get Started</Text>
+                <MaterialIcons name="navigate-next" color="#fff" size={20} />
+              </LinearGradient>
+            </TouchableOpacity>
+          </View>
         </View>
       </Animatable.View>
     </View>
@@ -77,6 +82,14 @@ const styles = StyleSheet.create({
     height: height_logo,
   },
   title: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  title2: {
     color: '#05375a',
     fontSize: 30,
     fontWeight: 'bold',
@@ -84,6 +97,7 @@ const styles = StyleSheet.create({
   text: {
     color: 'grey',
     marginTop: 5,
+    fontSize: 20,
   },
   button: {
     alignItems: 'flex-end',
@@ -96,9 +110,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 50,
     flexDirection: 'row',
+    marginLeft: 20,
+    // marginBottom: 2000,
   },
   textSign: {
     color: 'white',
     fontWeight: 'bold',
+  },
+  views: {
+    flexDirection: 'row',
+    marginTop: 40,
+  },
+  views2: {
+    flexDirection: 'column',
+    marginTop: 20,
   },
 });
